6a97c51054cef64e5f50a750cacd647a8c6f1ea4
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-05-02 00:58:08 +0300

    added schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
+
+schema_view = get_schema_view(title='Pasttbin API')
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url('', include(router.urls)),
 ]
